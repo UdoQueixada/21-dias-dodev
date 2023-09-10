af1792b69de41156ae8c40a5c44de2026828dad0
Udo Queixada <udoqueixada@gmail.com>
2023-09-10 19:23:10 +0100

    Desafio dia 3

--- dia-3/dia3.js
+++ dia-3/dia3.js
@@ -1,3 +1,5 @@
+//DESAFIO DIA #3: Conhecendo o usuário
+//1 - Crie um sistema que peça para o seu usuário as seguintes informações: nome, idade, altura e peso. 
 //2 - Salve essas informações em variáveis e a partir delas obtenha o ano de nascimento (considere o ano atual como 2023, desconsidere o mês) e o IMC do usuário.
 // *O IMC é a relação entre peso e altura e o cálculo é feito de acordo com a fórmula: 
 //3 - Exiba no console a seguinte mensagem (substituindo pelas informações obtidas): "Olá giovanni, você tem 23 anos, nasceu em 2000, tem 1,83 de altura, pesa 75kg seu IMC é 22,39 Kg/m2" 
